2f289da1edbf681ab9598297b5ac4d2c2b890222
saulcoops <saulcoops@gmail.com>
2020-06-11 14:28:12 +0100

    added fractal zoom feature

--- scripts/fractal.js
+++ scripts/fractal.js
@@ -1,50 +1,97 @@
-var iter = 30;
+var iter = 150;
+var vx1 = -2;
+var vy1 = -2;
+var vx2 = 2;
+var vy2 = 2;
+var drawboxX = 0;
+var drawboxY = 0;
+var dragging = false;
+var started = false;
+var cRe;
+var cIm;
+
+
+function mapping(s,a1,a2,b1,b2){
+  return (b1 + ((s-a1)*(b2-b1))/(a2-a1));
+}
+
+window.addEventListener("beforeunload", function(e){
+  document.getElementById('imaginary').value = "";
+  document.getElementById('real').value = "";
+}, false);
+
 function setup(){
   if(document.getElementById('real').value != ""){
+    colorMode(HSB, iter);
+    strokeWeight(4);
+    stroke(0);
     let c = createCanvas(700,700);
+    started = true;
     drawStuff(parseFloat(document.getElementById('real').value),parseFloat(document.getElementById('imaginary').value));
     document.getElementById('imaginary').value = "";
     document.getElementById('real').value = "";
-    saveCanvas(c, 'myCanvas', 'jpg');
+    //saveCanvas(c, 'myCanvas', 'jpg');
+  }
+  //drawStuff();
+}
+
+function mousePressed() {
+  if(mouseX >= 0  && mouseX <= 700 && mouseY >= 0  && mouseY <= 700){
+    if(started){
+      drawboxX = mouseX;
+      drawboxY = mouseY;
+      dragging = true;
+      console.log('clicked');
+    }
   }
 }
+
+function mouseDragged() {
+  if(started && dragging){
+    drawStuff(cRe,cIm);
+    fill('#f004');
+    rect(drawboxX, drawboxY, Math.abs(drawboxX-mouseX), Math.abs(drawboxX-mouseX));
+  }
+}
+
+function mouseReleased() {
+  if(started && dragging){
+    dragging = false;
+    vx1 = map(drawboxX,0,700,vx1,vx2);
+    vx2 = map(mouseX,0,700,vx1,vx2);
+    vy1 = map(drawboxY,0,700,vy1,vy2);
+    vy2 = map(mouseY,0,700,vy1,vy2);
+    console.log('PRINTING-----------');
+    drawStuff(cRe,cIm);
+  }
+}
+
+
 function drawStuff(a,b){
-  var cRe = a;
-  var cIm = b;
+  cRe = a;
+  cIm = b;
   var currentX = (mouseX-width/2)/100;
   var currentY = (mouseY-width/2)/100;
   background(0,0,0);
   strokeWeight(2);
-  translate(width/2,height/2);
   var a = new Complex(0,0);
   var initialRe = currentX;
   var initialIm = currentY;
   var prevRe = 1000;
   var prevIm = 1000;
-  for(var x = -5; x < 6;x+= 0.01){
-    for(var y = -5; y < 6;y+=0.01){
-      a = new Complex(x,y);
+  for(var x = 0; x < 701;x+= 1){
+    for(var y = 0; y < 701;y+=1){
+      a = new Complex(mapping(x,0,700,vx1,vx2),mapping(y,0,700,vy1,vy2));
       for(var i = 0; i < iter; i++){
         a = (a.mult(a));
         a.re += cRe;
         a.im += cIm;
-        // line(initialRe * 100,initialIm * 100,a.re * 100,a.im * 100)
-        // var initialRe = a.re;
-        // var initialIm = a.im;
-        if(Math.abs(a.re) > 5 && Math.abs(a.im) > 5){
-          stroke(min((i+1)*255/(iter),255),0,0);
-          point(x * 100,y * 100);
-          break;
-        }
-        if(Math.sqrt((0-a.re)**2 + (0-a.im)**2) < 0.1){
-          stroke(0,min((i+1)*255/(iter),255),0);
-          point(x * 100,y * 100);
+        if(Math.abs(a.re) >100 && Math.abs(a.im) > 100){
+          set(x, y, color(i, iter, iter));
           break;
         }
       }
-      // if(Math.abs(a.re) * 100 < width/2 && Math.abs(a.im) * 100 < width/2){
-      //   point(x,y);
-      // }
     }
   }
+  updatePixels();
 }
