e5e7b31fc6be3195daeb8a6d3c1986c34ee51393
saulcoops <saulcoops@gmail.com>
2020-09-24 22:52:23 +0100

    sql

--- beemessenger/messageuser.php
+++ beemessenger/messageuser.php
@@ -5,7 +5,7 @@
     <link rel="shortcut icon" type="image/png" href="https://freeiconshop.com/wp-content/uploads/edd/documents-outline.png" />
     <link rel="stylesheet" href="registerstyle.css">
     <title>Login 🐝</title>
-    <script src="http://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="messageuserstyle.css">
   </head>
   <body class="formvalidator">
