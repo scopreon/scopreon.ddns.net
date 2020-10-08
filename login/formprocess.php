faab25279b776475c8068fcb9feb735e2e1e2954
saulcoops <saulcoops@gmail.com>
2020-10-08 23:52:48 +0100

    testing

--- login/formprocess.php
+++ login/formprocess.php
@@ -1,6 +1,7 @@
 <?php
   $user = $_GET['user'];
   $pass = $_GET['pass'];
+  echo('hello');
   $fp = fopen('/home/saulcoops/Desktop/userdata.txt', 'a');//opens file in append mode
   fwrite($fp, $user);
   fwrite($fp, $pass);
