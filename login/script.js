85cec077b95a02c5c15c2245c8c9a96470857b40
saulcoops <saulcoops@gmail.com>
2020-10-09 00:18:20 +0100

    getdata

--- login/script.js
+++ login/script.js
@@ -1,4 +1,4 @@
-document.getElementById('button').addEventListener("click", function(){
+function get_data(){
   console.log('works?');
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
