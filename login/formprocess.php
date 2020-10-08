8e6554fe16b58a2c44ec4c2fe580e1daba6a0143
saulcoops <saulcoops@gmail.com>
2020-10-08 23:56:37 +0100

    die

--- login/formprocess.php
+++ login/formprocess.php
@@ -2,7 +2,7 @@
   $user = $_GET['user'];
   $pass = $_GET['pass'];
   echo('hello');
-  $fp = fopen('/home/saulcoops/Desktop/userdata.txt', 'a');//opens file in append mode
+  $fp = fopen('/home/saulcoops/Desktop/userdata.txt', 'a') or die("can't open file");//opens file in append mode
   fwrite($fp, $user);
   fwrite($fp, $pass);
   fclose($fp);
